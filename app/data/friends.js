af8e91b65eb197b1efb67b710e3321f9ba2746b9
Carlitos de Castro <decastrocarlitos@gmail.com>
2018-08-31 03:18:14 -0400

    Created Assignment

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+    {
+        name: "Dr.Strange",
+        photo: "https://media.tenor.com/images/a30701b3f6a9ef634f147eab8c57fab7/tenor.png",
+        scores: [5, 1, 1, 2, 5, 2, 3, 1, 4, 5]
+    },
+    {
+        name: "Tony Stark",
+        photo: "https://vignette.wikia.nocookie.net/thegamesrp/images/b/b5/Tony_stark.jpg/revision/latest?cb=20141124042521",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Gamora",
+        photo: "https://vignette.wikia.nocookie.net/disney-marvel-gotg/images/6/65/GamoraGOTG2.png/revision/latest?cb=20170607214146",
+        scores: [2, 4, 2, 1, 5, 3, 1, 2, 4, 1]
+    },
+    {
+        name: "Natasha",
+        photo: "https://s1.r29static.com//bin/entry/1b5/340x408,80/1996479/image.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Peter Parker",
+        photo: "https://t00.deviantart.net/AhgfAKyIpY2pO4O6ekPV4ATBEmU=/fit-in/300x900/filters:no_upscale():origin()/pre00/5419/th/pre/f/2017/208/d/3/a_night_to_remember__peter_x_reader__1_3_by_savrom-dbhumss.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+]
+module.exports = friends;
